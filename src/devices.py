a4bcb1af79a1393dd7ab1fe0c606c136c57d5849
Steve Goldsmith <steve@aurifexlabs.com>
2023-01-03 11:38:40 -0700

    Fix.

--- src/devices.py
+++ src/devices.py
@@ -10,26 +10,26 @@ def get_devices(config):
 
     from drivers.periphery.button import Button
     devices['fan_button'] = {
-        'read': Button(config.buttons.fan).read
+        'read': Button(config["buttons"]["fan"]).read
     }
     devices['alarm_reset_button'] = {
-        'read': Button(config.buttons.alarm_reset).read
+        'read': Button(config["buttons"]["alarm_reset"]).read
     }
 
     from drivers.periphery.fan import Fan
     devices['fan'] = {
-        'write': Fan(config.fan).write
+        'write': Fan(config["fan"]).write
     }
 
     from drivers.periphery.led import Led
     devices['warning_led'] = {
-        'write': Led(config.leds.warning).write
+        'write': Led(config["leds"]["warning"]).write
     }
     devices['alarm_led_0'] = {
-        'write': Led(config.leds.alarm_0).write
+        'write': Led(config["leds"]["alarm_0"]).write
     }
     devices['alarm_led_1'] = {
-        'write': Led(config.leds.alarm_1).write
+        'write': Led(config["leds"]["alarm_1"]).write
     }
 
     return devices
