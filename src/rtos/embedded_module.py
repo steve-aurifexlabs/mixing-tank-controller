21395e4a51ece5b1a5e3d9c97afc277910c904d5
Steve Goldsmith <steve@aurifexlabs.com>
2023-01-03 13:29:53 -0700

    Fix.

--- src/rtos/embedded_module.py
+++ src/rtos/embedded_module.py
@@ -35,7 +35,6 @@ class EmbeddedModule(object):
 
             self.is_idle = False
             while not self.is_idle:
-                print('no step? ', self.step)
                 await self.step()
 
             self.queue.task_done()
